bedec6831302924bff48f3b72a568c3881bfbed0
ssun <pros5018@nate.com>
2019-02-09 18:28:30 +0900

    4주차- dialog 생성

---
+++ src/app/admin/manage-hero/manage-dialog/manage-dialog.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-manage-dialog',
+  templateUrl: './manage-dialog.component.html',
+  styleUrls: ['./manage-dialog.component.scss']
+})
+export class ManageDialogComponent implements OnInit {
+  @Input()
+  name: string;
+
+  constructor(private activeModal: NgbActiveModal) { }
+
+  ngOnInit() {
+  }
+
+}
